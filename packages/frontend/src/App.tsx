297b22697fc389c49853c8354c8c354521461303
w-xuefeng <xuefeng@kcalb.wang>
2023-03-22 10:09:26 +0000

    feat: initial frontend project

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import React from 'react';
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App" role="main">
+      <article className="App-article">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>Welcome to React!</h3>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </article>
+    </div>
+  );
+}
+
+export default App;
