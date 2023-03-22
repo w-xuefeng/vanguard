a0190aa27a2dc29e7c3171a6ce98be561bef3dfc
w-xuefeng <xuefeng@kcalb.wang>
2023-03-22 10:39:38 +0000

    feat: initial frontend project

--- packages/frontend/src/reportWebVitals.ts
+++ packages/frontend/src/reportWebVitals.ts
@@ -1,13 +1,11 @@
-const reportWebVitals = (onPerfEntry: any) => {
+import { onCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals'
+const reportWebVitals = (onPerfEntry: (data: Record<string, any>) => void) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
+    onCLS(onPerfEntry);
+    onFID(onPerfEntry);
+    onFCP(onPerfEntry);
+    onLCP(onPerfEntry);
+    onTTFB(onPerfEntry);
+  };
 };
-
 export default reportWebVitals;
