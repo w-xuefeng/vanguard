c0db37b1d603e134c967b5309caec67c78a649f0
XueFLY <xuefeng@kcalb.wang>
2023-03-22 20:11:06 +0800

    feat: init frontend project

---
+++ check-bun.sh
@@ -0,0 +1,12 @@
+if hash bun 2>/dev/null; then
+  v=$(bun --version)
+  echo "👌 Bun version: $v"
+  echo '🥰 Bun has been installed before!'
+else
+  echo '🚀 Start installing Bun ...'
+  curl -fsSL https://bun.sh/install | bash
+  sudo ln -s $HOME/.bun/bin/bun /usr/bin
+  echo '🥰 Bun has been installed!'
+  v=$(bun --version)
+  echo "👌 Bun version: $v"
+fi
