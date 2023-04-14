2aaea35093534a29b767bcf79a470e7041487222
XueFLY <xuefeng@kcalb.wang>
2023-04-14 20:23:28 +0800

    fix: add theme for layout

--- packages/frontend/src/layouts/index.tsx
+++ packages/frontend/src/layouts/index.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from "umi";
 import styles from "./index.less";
+import { ConfigProvider } from "antd";
+import { theme } from "@/config";
 
 export interface ILayoutProps {}
 
 export default function Layout(_props: ILayoutProps) {
   return (
-    <div className={styles.layout}>
-      <Outlet />
-    </div>
+    <ConfigProvider theme={theme}>
+      <div className={styles.layout}>
+        <Outlet />
+      </div>
+    </ConfigProvider>
   );
 }
