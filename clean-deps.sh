2231c1fc6e5d75b6785efc0f3c4101447567e4b3
XueFLY <xuefeng@kcalb.wang>
2023-04-15 12:46:22 +0800

    chore: modify build shell

--- clean-deps.sh
+++ clean-deps.sh
@@ -1,2 +1,12 @@
-rm -rf node_modules
-rm -rf packages/*/node_modules
+#!/bin/sh
+
+lockFile="bun.lockb"
+rootDepsDir="node_modules"
+packagesDepsDir="packages/*/node_modules"
+
+if [ -f $lockFile ]; then
+  rm $lockFile
+fi
+
+rm -rf $rootDepsDir
+rm -rf $packagesDepsDir
--- generate.sh
+++ generate.sh
@@ -1,6 +1,11 @@
-rm -rf dist
-mkdir dist
-cp -r packages/backend/ dist
-cp check-bun.sh dist/check-bun.sh
-rm -rf dist/runtime
-echo "sh check-bun.sh && bun bun-i && bun serve" >>dist/vanguard.sh
+#!/bin/sh
+
+target="dist"
+backend="packages/backend/"
+
+rm -rf $target
+mkdir $target
+cp -r $backend $target
+cp check-bun.sh $target/check-bun.sh
+rm -rf $target/runtime
+echo "sh check-bun.sh && bun bun-i && bun serve" >>$target/vanguard.sh
