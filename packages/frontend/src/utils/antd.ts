c0ba35f4e7298f74ce242b21ef02165bfd5fa006
XueFLY <xuefeng@kcalb.wang>
2023-04-16 12:54:42 +0800

    fix: fixed antd useApp

--- packages/frontend/src/utils/antd.ts
+++ packages/frontend/src/utils/antd.ts
@@ -1,7 +1,1 @@
-import { notification as antdNotification } from 'antd';
-import { catchError } from '.';
-
-export const notification = Object.keys(antdNotification).reduce((t, ck) => {
-  t[ck as keyof typeof antdNotification] = (config: any) => catchError(antdNotification[ck as keyof typeof antdNotification], [config])
-  return t;
-}, {} as typeof antdNotification)
+export { notification } from 'antd';
