1d1188b40a08eb8b0070232663411ae80455f782
XueFLY <xuefeng@kcalb.wang>
2023-04-22 22:02:28 +0800

    chore: modify generate.sh

--- generate.sh
+++ generate.sh
@@ -8,4 +8,4 @@ mkdir $target
 cp -r $backend $target
 cp check-bun.sh $target/check-bun.sh
 rm -rf $target/runtime
-echo "sh check-bun.sh && bun i && bun serve" >>$target/vanguard.sh
+echo "sh check-bun.sh && bun i && bun run serve" >>$target/vanguard.sh
