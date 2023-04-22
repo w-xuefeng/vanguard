28f33bea8ebe8aaa2e2bf144f062b95a74e232ef
XueFLY <xuefeng@kcalb.wang>
2023-04-22 22:43:13 +0800

    chore: modify generate.sh

--- generate.sh
+++ generate.sh
@@ -8,5 +8,4 @@ mkdir $target
 cp -r $backend $target
 cp check-bun.sh $target/check-bun.sh
 rm -rf $target/runtime
-npm install pm2 -g
 echo "sh check-bun.sh && bun i && bun serve" >>$target/vanguard.sh
